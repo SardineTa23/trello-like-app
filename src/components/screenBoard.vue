186e30d839e910041cecf2b13c91d9be81dcac6c
k-iwashita-prtimes <wonderland.7878@gmail.com>
2022-02-20 23:18:32 +0900

    init commit

---
+++ src/components/screenBoard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <header>Trello Like App</header>
+    <main>
+      <p class="info-line">All: 0 tasks</p>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
