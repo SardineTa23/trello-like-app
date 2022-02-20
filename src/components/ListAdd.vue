511189f950b3121347ae053e823d4bd768dbe8bf
k-iwashita-prtimes <wonderland.7878@gmail.com>
2022-02-21 00:14:38 +0900

    v-bindを使ってみる

--- src/components/ListAdd.vue
+++ src/components/ListAdd.vue
@@ -2,14 +2,18 @@
   <!-- サブミットイベント時にaddListメソッドをハンドル -->
   <!-- @はv-onの省略記法 -->
   <!-- .preventはVueのイベント修飾詞であり、サブミット時にリロードされるのを防ぐ -->
-  <form class="addlist" @submit.prevent="addList">
+  <form :class="classList" @submit.prevent="addList">
     <input
       v-model="title"
       type="text"
       class="text-input"
       placeholder="Add new list"
+      @focusin="startEditing"
+      @focusout="finishEditing"
     />
-    <button type="submit" class="add-button">Add</button>
+    <button type="submit" class="add-button" v-if="isEditing || titleExists">
+      Add
+    </button>
   </form>
 </template>
 
@@ -18,13 +22,38 @@ export default {
   data: function () {
     return {
       title: "",
+      isEditing: false,
     };
   },
-   methods: {
-    addList: function() {
-      this.$store.dispatch('addlist', { title: this.title })
-      this.title = ''
+  computed: {
+    // return array
+    classList() {
+      const classList = ["addlist"];
+      if (this.isEditing) {
+        classList.push("active");
+      }
+      if (this.titleExists) {
+        classList.push("addable");
+      }
+      return classList;
     },
-  }
+
+    // return bool
+    titleExists() {
+      return this.title.length > 0;
+    },
+  },
+  methods: {
+    addList: function () {
+      this.$store.dispatch("addlist", { title: this.title });
+      this.title = "";
+    },
+    startEditing: function () {
+      this.isEditing = true;
+    },
+    finishEditing: function () {
+      this.isEditing = false;
+    },
+  },
 };
 </script>
