2a4c2e2a75d72468a880fbe68d51d2b44e1982cd
k-iwashita-prtimes <wonderland.7878@gmail.com>
2022-02-21 00:34:43 +0900

    コメントの追加

--- src/components/screenBoard.vue
+++ src/components/screenBoard.vue
@@ -3,16 +3,35 @@
     <header>Trello Like App</header>
     <main>
       <p class="info-line">All: 0 tasks</p>
-      <list-add />
+      <div class="list-index">
+        <!-- v-bind(:hoge)でバインドさせたコンポーネントにデータを受け渡すことができる -->
+        <task-list
+          v-for="(item, index) in lists"
+          :key="item.id"
+          :title="item.title"
+          :listIndex="index"
+        />
+        <task-list-add />
+      </div>
     </main>
   </div>
 </template>
 
 <script>
-import ListAdd from './ListAdd.vue'
+import TaskListAdd from "./TaskListAdd.vue";
+import TaskList from "./TaskList";
+import { mapState } from "vuex";
+
 export default {
   components: {
-    ListAdd
+    TaskListAdd,
+    TaskList,
+  },
+  computed: {
+    ...mapState([
+      // stateのlistsを参照
+      'lists'
+    ]),
   },
 };
 </script>
